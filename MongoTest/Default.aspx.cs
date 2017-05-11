87485768d21a75022298f8e60923ecad1f4a7656
Steve Conger <steve@spconger.com>
2017-05-11 14:50:47 -0700

    Commit MongoTest 1

---
+++ MongoTest/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var mongo = new MongoClient();
+        var db = mongo.GetDatabase("techStore");
+        var productCollection= db.GetCollection<BsonDocument>("productCollection");
+        var documents  = productCollection.Find(new BsonDocument()).ToList();
+        List<string> productNames = new List<string>();
+        foreach(var d in documents)
+        {
+            string prod = d.GetElement("product").ToString();
+            int i = prod.IndexOf("=");
+            prod = prod.Substring(i + 1);
+            productNames.Add(prod);
+            
+            
+        }
+        GridView1.DataSource = productNames;
+        GridView1.DataBind();
+
+    }
+}
